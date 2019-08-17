4b7412e0ea61d4a02c98c2931369220718496f77
w00x <w00x@sibmail.com>
2019-08-17 23:43:47 +0700

    add init script

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir --parents /home/box/web/public/img
+mkdir --parents /home/box/web/public/css
+mkdir --parents /home/box/web/public/js
+mkdir --parents /home/box/web/uploads
+mkdir --parents /home/box/web/etc
+
+cp -rf ./nginx.conf /etc/nginx/
