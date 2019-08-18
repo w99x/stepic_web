0b26a648778bc2d5b602a98c333d230203394db3
w00x <w00x@sibmail.com>
2019-08-18 19:05:40 +0700

    Fix init sript

--- django/init.sh
+++ django/init.sh
@@ -4,7 +4,6 @@ etcdir=/home/box/etc
 mkdir --parents $webdir
 mkdir --parents $etcdir
 
-cp -rf ./hello_app.py $webdir/hello.py
 cp -rf ./gunicorn_conf.py $etcdir/gunicorn.py
 cp -rf ./nginx.conf $etcdir/
 cp -rf ./ask $webdir/ask
@@ -13,5 +12,6 @@ sudo ln -sf $etcdir/gunicorn.py  /etc/gunicorn.d/gunicorn.py
 sudo ln -sf $etcdir/nginx.conf /etc/nginx/sites-enabled/default
 
 sudo /etc/init.d/nginx restart
-sudo /etc/init.d/gunicorn restart $webdir/ask/ask/wsgi.py -c $etcdir/gunicorn.py 
+sudo /etc/init.d/gunicorn restart 
+#$webdir/ask/ask/wsgi.py -c $etcdir/gunicorn.py 
 sudo /etc/init.d/mysql start
