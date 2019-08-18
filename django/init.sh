d0a6bea2b10b3b646ee1eabe02737d0c6a3c3311
w00x <w00x@sibmail.com>
2019-08-18 18:21:45 +0700

    add django project

---
+++ django/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+webdir=/home/box/web
+etcdir=/home/box/etc
+mkdir --parents $webdir
+mkdir --parents $etcdir
+
+cp -rf ./hello_app.py $webdir/hello.py
+cp -rf ./gunicorn_conf.py $etcdir/gunicorn.py
+cp -rf ./nginx.conf $etcdir/
+cp -rf ./ask $webdir/ask
+
+sudo ln -sf $etcdir/gunicorn.py  /etc/gunicorn.d/gunicorn.py
+sudo ln -sf $etcdir/nginx.conf /etc/nginx/sites-enabled/default
+
+sudo /etc/init.d/nginx restart
+sudo /etc/init.d/gunicorn restart $webdir/ask/ask/wsgi.py -c $etcdir/gunicorn.py 
+sudo /etc/init.d/mysql start
