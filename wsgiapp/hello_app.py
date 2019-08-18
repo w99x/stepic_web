b6c9346a1b04d7a6613a2accce245e2503ed3790
w00x <w00x@sibmail.com>
2019-08-18 12:00:42 +0700

    Encoder out string

--- wsgiapp/hello_app.py
+++ wsgiapp/hello_app.py
@@ -7,4 +7,4 @@ def wsgi_application(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
     start_response(status, headers )     
-    return [ body ]
+    return [ str.encode(body) ]
