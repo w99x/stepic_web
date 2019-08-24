21f5e8b803d6d751b7bc9e0425b5c5666e58d2e7
w00x <w00x@sibmail.com>
2019-08-24 14:50:49 +0700

    more fixes in models

--- mysql/ask/qa/models.py
+++ mysql/ask/qa/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=250)
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField()
+    added_at = models.DateTimeField(blank = True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name='question_like_user')
     class Meta:
@@ -22,6 +22,6 @@ class QuestionManager():
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateTimeField(blank = True, auto_now_add=True)
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     author = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)    
